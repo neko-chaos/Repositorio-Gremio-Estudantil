416d74c0672571eb8f47d5766316ecc01550c79e
Neko Chaos <129768901+neko-chaos@users.noreply.github.com>
2023-11-27 11:50:23 -0300

    Add files via upload
    IM DED

--- css/main.js
+++ css/main.js
@@ -1,5 +1,46 @@
 function tocarSomAplausos(){
-document.querySelector('#som_tecla_aplausos').play();
-}
+ document.querySelector('#som_tecla_aplausos').play();
+ }
+ document.querySelector('.tecla_aplausos').onclick = function(){}
 
-document.querySelector('.tecla_aplausos').onclick = function(){}
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_vaia').play();
+    }
+    document.querySelector('.tecla_vaia').onclick = function(){}
+
+function tocarSomAplausos(){
+   document.querySelector('#som_tecla_risada').play();
+      }
+    document.querySelector('.tecla_risada').onclick = function(){}
+
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_grilo').play();
+     }
+    document.querySelector('.tecla_grilo').onclick = function(){}
+
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_errado').play();
+     }
+    document.querySelector('.tecla_errado').onclick = function(){}
+
+function tocarSomAplausos(){
+   document.querySelector('#som_tecla_dinheiro').play();
+     }
+  document.querySelector('.tecla_dinheiro').onclick = function(){}
+
+function tocarSomAplausos(){
+   document.querySelector('#som_tecla_tambores').play();
+     }
+  document.querySelector('.tecla_tambores').onclick = function(){}
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_vitoria').play();
+     }
+    document.querySelector('.tecla_vitoria').onclick = function(){}
+
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_trombeta').play();
+     }
+    document.querySelector('.tecla_trombeta').onclick = function(){}
+    
+    
+    
